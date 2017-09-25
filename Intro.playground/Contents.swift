59840c93e7052ffc6f46e7c73976c1ae50c114e9
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-25 08:32:43 -0600

    Added a bunch of variables to test the functionality of variables in swift.

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -1,5 +1,27 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-var str = "Hello, playground"
+//Explicit Type declaration with no value
+var ramen :String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 54322456
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Matthew"
+print(myName)
+//myName = soup
+//Can't assign int a let once assigned
+var test = 157143852
+test = test * myAge
