6cbb824e29e7c2d1eb4756335b842999c2cab46b
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-25 09:05:05 -0600

    Started to test strings in a strings playground.

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,9 @@
+    //: Playground - noun: a place where people can play
+
+import UIKit
+let singleLine = "This is a one line string"
+print(singleLine)
+let multiLine = """
+This is the same as the line above.
+"""
+
