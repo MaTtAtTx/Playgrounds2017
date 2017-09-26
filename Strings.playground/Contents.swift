f840e675dab569711c1f639ae89c7cfa9901cb90
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-26 08:40:19 -0600

    Added more info. to strings.

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -5,3 +5,7 @@ let singleLine = "These are the same."
 //let multiline = """
 //This is the same as the line above just on multiple lines.
 //"""
+
+//String Interpolation
+let multiplier = 55
+let message = "\(multiplier) times 750.525 is \(Double(multiplier) * 750.525)"
