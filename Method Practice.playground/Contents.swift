92d7850cf089ab46a980d7fbffc493fa7b401672
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-27 08:41:14 -0600

    Tested more methods and also tested if else and while.

--- Method Practice.playground/Contents.swift
+++ Method Practice.playground/Contents.swift
@@ -19,4 +19,34 @@ public func evenLessSimpleMethod(count :Int, bandName :String, date :String) -> 
     let info = "I have seen " + bandName + " \(count) times since \(date)"
     print(info)
 }
-evenLessSimpleMethod(count: 200, bandName: "A Random Band", date: "July, 2008")
+evenLessSimpleMethod(count: 200, bandName: "a random band", date: "July, 2008")
+
+public func calculateDays(first firstDay :Int, secondDay :Int) -> Int
+{
+    let answer = secondDay - firstDay
+    
+    return answer
+}
+calculateDays(first: 4, secondDay: 5)
+
+public func name(friendName homeName :String) -> Void
+{
+    let info = "At home my name is " + homeName
+    print(info)
+}
+name(friendName: "Matthew")
+
+var guess = 444
+if (guess < 500)
+{
+    print("Nice")
+}
+else if (guess == 17)
+{
+    print("Amazing")
+}
+while (guess > 0)
+{
+    print("????????? \(guess)")
+    guess -= 1
+}
